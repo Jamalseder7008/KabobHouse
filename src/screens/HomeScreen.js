24eff293fdfad55c20d988011fe4a492bf1927c3
Joshua <jasncla2020@gmail.com>
2021-11-19 15:07:57 -0600

    Updated Homepage, logo still not centered

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,33 +1,77 @@
 import React from "react";
-import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity, Flatlist, AppRegistry } from "react-native";
+import ImageDetail from "../components/ImageDetail";
+
 
 const HomeScreen = (props) => {
   
-
   return <View>
-    <Text style={styles.text}>KABOB HOUSE {"\n\n"}
-      View the menu{"\n\n"} View the map</Text>
-    <TouchableOpacity onPress={() => { props.navigation.navigate("Components")}}>
-      <Text>ComponentsScreen</Text> 
+  <ImageDetail style={styles.container} imageSource={require('../../assets/KabobHouseLogo.jpg')}/>
+    <Text style={styles.text}>KABOB HOUSE </Text>
+    <TouchableOpacity onPress={() => { props.navigation.navigate("Image")}}>
+      <Text style={styles.text2}>FULL MENU </Text> 
     </TouchableOpacity>
-    <TouchableOpacity onPress={() => {props.navigation.navigate("List")}}>
-      <Text>ListScreen</Text>
+    <TouchableOpacity onPress={() => {props.navigation.navigate("Image")}}>
+      <Text style={styles.text3}>APPETIZERS </Text>
     </TouchableOpacity>
     <TouchableOpacity onPress = {() => {props.navigation.navigate("Image")}}>
-      <Text>Image Screen</Text>
+      <Text style={styles.text}>PLATES </Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress = {() => {props.navigation.navigate("Image")}}>
+      <Text style={styles.text2}>DRINKS </Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress = {() => {props.navigation.navigate("Image")}}>
+      <Text style={styles.text3}>DESSERTS </Text>
     </TouchableOpacity>
     <Button 
       onPress={function(){ props.navigation.navigate("Cart")}}
-      title={"Cart"}
+      title={"PHONE"}
     />
+    <Button 
+      onPress={function(){ props.navigation.navigate("Cart")}}
+      title={"CART"}
+    />
+    <Button 
+      onPress={function(){ props.navigation.navigate("Map")}}
+      title={"MAP"}
+    />
+
   </View>
 };
 
 const styles = StyleSheet.create({
   
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+    textAlign: 'center',
+    backgroundColor: '#007A3D',
+    textShadowColor: '#fff',
+    textShadowRadius: 20
+  },
+  text2: {
+    fontSize: 30,
+    textAlign: 'center',
+    backgroundColor: '#007A3D',
+    color: '#CE1126',
+    textShadowColor: '#fff',
+    textShadowRadius: 20
+   },
+   text3: {
+    fontSize: 30,
+    textAlign: 'center',
+    backgroundColor: '#007A3D',
+    color: '#fff',
+    textShadowColor: '#fff',
+    textShadowRadius: 20
+   },
+   container: {
+    justifyContent: 'center',
+    alignItems: 'center'
+    },
+   logo:   {
+        width: 300,
+        height: 400
+    }
 });
 
 export default HomeScreen;
